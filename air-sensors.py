bf36c4635f6493d9a059e2b03816b3e70f4220e8
joel-lbth <110038598+joel-lbth@users.noreply.github.com>
2023-03-22 15:16:39 +0000

    don't try to rename columns that don't exist

--- air-sensors.py
+++ air-sensors.py
@@ -18,11 +18,6 @@ req = requests.get("https://api.erg.ic.ac.uk/AirQuality/Information/MonitoringSi
 js = req.json()
 sites = js['Sites']['Site']
 db['sites'].upsert_all(sites,pk=('@SiteCode'))
-try:
-    db['sites'].transform(rename={"@Longitude": "Longitude"})
-    db['sites'].transform(rename={"@Latitude": "Latitude"})
-except:
-    print('sites table already have column names in right coordinates')
 
 # PREPARE TO SCAN DATA FOR THE LAST 1 WEEK
 EndDate = date.today() + timedelta(days = 1)
