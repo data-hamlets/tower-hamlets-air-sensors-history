fc6180abca033a32110cdda95502779acfee7fa5
joel <joseph.leach@towerhamlets.gov.uk>
2024-10-24 22:35:50 +0100

    improve variable naming and use

--- air-sensors.py
+++ air-sensors.py
@@ -9,7 +9,7 @@ db = sqlite_utils.Database("air-sensors.db", recreate=False)
 # EXTRACT THE SITES IN TOWER HAMLETS
 db["sites"].drop(ignore=True)
 req = requests.get(
-    "https://api.erg.ic.ac.uk/AirQuality/Information/MonitoringSiteSpecies/GroupName=towerhamlets/Json"
+    "https://api.erg.ic.ac.uk/AirQuality/Information/MonitoringSiteSpecies/GroupName=towerhamlets/Json", verify=verify
 )
 js = req.json()
 sites = js["Sites"]["Site"]
@@ -32,13 +32,16 @@ current_date = date.today()
 # Number of days to step back
 days_to_step_back = 1
 
+# Whether t o check certificate
+verify=True
+
 for i in range(days_to_step_back, -1, -1):
     # Calculate the date that is one day ago
     previous_date = current_date - timedelta(days=1)
     
-    # PREPARE TO SCAN DATA FOR THE LAST 1 WEEK
+    # PREPARE TO SCAN DATA FOR THE LAST 1 EPOCH
     EndDate = previous_date + timedelta(days=1)
-    EndWeekDate = EndDate
+    EndEpochDate = EndDate
     StartEpochDate = EndDate - timedelta(days=1)
     StartDate = StartEpochDate - timedelta(days=1)
 
@@ -51,9 +54,9 @@ for i in range(days_to_step_back, -1, -1):
                 l["@SiteCode"] = el["@SiteCode"]
                 return l
 
-            url = f'https://api.erg.ic.ac.uk/AirQuality/Data/SiteSpecies/SiteCode={el["@SiteCode"]}/SpeciesCode=NO2/StartDate={StartEpochDate.strftime("%d %b %Y")}/EndDate={EndWeekDate.strftime("%d %b %Y")}/Json'
-            print(url)
-            req = requests.get(url, headers={"Connection": "close"})
+            url = f'https://api.erg.ic.ac.uk/AirQuality/Data/SiteSpecies/SiteCode={el["@SiteCode"]}/SpeciesCode={tablename}/StartDate={StartEpochDate.strftime("%d %b %Y")}/EndDate={EndEpochDate.strftime("%d %b %Y")}/Json'
+
+            req = requests.get(url, headers={"Connection": "close"}, verify=verify)
             j = req.json()
             # CLEAN SITES WITH NO DATA OR ZERO VALUE OR NOT NO2 (ONLY MEASURE AVAILABLE AT ALL SITES)
             filtered = [
@@ -65,8 +68,8 @@ for i in range(days_to_step_back, -1, -1):
                 filtered = map(convert, filtered)
                 filteredList = list(filtered)
                 db[tablename].upsert_all(filteredList, pk=("@MeasurementDateGMT", "@SiteCode"))
-        EndWeekDate = StartEpochDate
-        StartEpochDate = EndWeekDate - timedelta(days=1)
+        EndEpochDate = StartEpochDate
+        StartEpochDate = EndEpochDate - timedelta(days=1)
 
     # at end of each for loop iteration
     # move current_date back a step preparing next iteration
